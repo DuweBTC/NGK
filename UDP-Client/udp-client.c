88d39de4938755285e86218f8069fb6c9e448974
TigrisDK <45198976+TigrisDK@users.noreply.github.com>
2022-03-14 12:03:23 +0100

    tilrettet UDP-client så det er delt op

--- UDP-Client/udp-client.c
+++ UDP-Client/udp-client.c
@@ -7,6 +7,14 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 
+int sockfd, portno;
+char buffer[256];
+
+void openFunc(char *argv[]);
+void connectFunc(char *argv[]);
+void writerFunc();
+void readerFunc();
+
 void error(const char *msg)
 {
     perror(msg);
@@ -15,43 +23,74 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-
-    char buffer[256];
-    if (argc < 3) {
+    if (argc < 3) 
+    {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
     }
+
+    openFunc(argv);
+    connectFunc(argv);
+    writerFunc();
+    readerFunc();
+
+    close(sockfd);
+    return 0;
+}
+
+void openFunc(char *argv[])
+{
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
+    {
         error("ERROR opening socket");
+    }
+}
+
+void connectFunc(char *argv[])
+{
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
     server = gethostbyname(argv[1]);
-    if (server == NULL) {
+    if (server == NULL) 
+    {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+    {
         error("ERROR connecting");
-    printf("Please enter the message: ");
+    }
+}
+
+void writerFunc()
+{
+    int n;
+    printf("For at få UDP uptime indtast 'U'\nFor at få UDP loadAVG indtast 'L'\n");
+    printf("Indtast kommando: ");
     bzero(buffer,256);
     fgets(buffer,255,stdin);
     n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
+    if (n < 0)
+    { 
          error("ERROR writing to socket");
+    }
+}
+
+void readerFunc()
+{
+    int n;
     bzero(buffer,256);
     n = read(sockfd,buffer,255);
-    if (n < 0) 
+    if (n < 0)
+    {
          error("ERROR reading from socket");
+    }
     printf("%s\n",buffer);
-    close(sockfd);
-    return 0;
 }
